7b4a03b5e335fd6533a9bba1513793bdc9d0140d
Cinton Yeboah <clintonyeb@gmail.com>
2020-07-08 00:51:44 -0500

    integrated demographics and content

---
+++ server/main/api/content.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import importlib
+from flask import Blueprint, jsonify
+
+from server.models.content.preprocess import preprocess
+from server.models.content.content_based_rec import ContentRecommender
+
+route = Blueprint('content', __name__)
+
+credits, keywords, links, links_small, metadata, ratings, ratings_small = preprocess.retrieve_datasets()
+smd = preprocess.create_content_ds(metadata, links_small, keywords, credits)
+content_rec = ContentRecommender(smd)
+
+@route.route("/api/content/<title>/<movie_id>")
+def recommend_content(title, movie_id):
+    return content_rec.recommend(title, movie_id).to_json(orient='records')
--- server/main/api/default.py
+++ server/main/api/default.py
@@ -1,28 +1,13 @@
 # -*- coding: utf-8 -*-
 """Default api blueprints for Demo application."""
 
-import pandas as pd
-import importlib
 from flask import Blueprint, jsonify
 
-import sys
-sys.path.append('ml')
-sys.path.append('data')
-import preprocess
-from content_based_rec import ContentRecommender
-
-
-credits, keywords, links, links_small, metadata, ratings, ratings_small = preprocess.retrieve_datasets()
-smd = preprocess.create_content_ds(metadata, links_small, keywords, credits)
-content_rec = ContentRecommender(smd)
-content_rec.recommend('Titanic', 3)
 route = Blueprint('default', __name__)
-
 
 @route.route("/api")
 def hello():
     return "Hello from Flask using Python 3.6.2!!"
-
 
 @route.route("/api/ping")
 def ping():
